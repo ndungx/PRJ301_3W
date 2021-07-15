efa5c491d2e653bc1284de491aa11e7dc613f6a6
ndungx <dpron12345@gmail.com>
2021-07-16 00:11:21 +0700

    Update google client id and client secret

--- src/java/com/ndungx/accessgoogle/common/Constants.java
+++ src/java/com/ndungx/accessgoogle/common/Constants.java
@@ -1,8 +1,8 @@
 package com.ndungx.accessgoogle.common;
 
 public class Constants {
-	public static String GOOGLE_CLIENT_ID = "225456220013-28j92le9f9urd61mjsc1vd8khnc1vuad.apps.googleusercontent.com";
-	public static String GOOGLE_CLIENT_SECRET = "Yr7SulJroMpMtOSUo0DTMYX7";
+	public static String GOOGLE_CLIENT_ID = "988633577351-53udkaq470dc83hmmd5vghr3mma0daqs.apps.googleusercontent.com";
+	public static String GOOGLE_CLIENT_SECRET = "KbhGIm-e382G4lqe118JVkJR";
 	public static String GOOGLE_REDIRECT_URI = "http://localhost:8084/PRJ301_3W/login-google";
 	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
 	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
